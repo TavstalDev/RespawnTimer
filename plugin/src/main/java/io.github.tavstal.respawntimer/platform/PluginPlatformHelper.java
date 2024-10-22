17e273e9c203d14508123cbd55c2c5c7b0e76888
Tavstal <tavstal@gmail.com>
2024-10-22 12:02:27 +0200

    Started implementing paper

---
+++ plugin/src/main/java/io.github.tavstal.respawntimer/platform/PluginPlatformHelper.java
@@ -0,0 +1,36 @@
+package io.github.tavstal.respawntimer.platform;
+
+import io.github.tavstal.respawntimer.platform.services.IPlatformHelper;
+
+public class PluginPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Paper";
+    }
+
+    // Note: plugin is always server side
+    @Override
+    public boolean isClientSide() {
+
+        return false;
+    }
+
+    @Override
+    public boolean isServerSide() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return false;
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return false;
+    }
+}
