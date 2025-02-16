e4ef63f4033f4b81d7dd3451c30c486cdf906698
TavstalDev <tavstal@gmail.com>
2025-02-16 03:30:48 +0100

    Fix

--- src/main/java/io/github/tavstal/respawntimer/utils/ChatUtils.java
+++ src/main/java/io/github/tavstal/respawntimer/utils/ChatUtils.java
@@ -113,8 +113,8 @@ public class ChatUtils {
             return MiniMessage.miniMessage().deserialize(message);
 
         // Convert '&' to '§' first (since ChatColor.stripColor requires '§')
-        String legacyColor = translateAlternateColorCodes(message);
-        return MiniMessage.miniMessage().deserialize(replacePlaceholders(legacyToMiniMessage(legacyColor)));
+        String legacyColor = translateAlternateColorCodes(replacePlaceholders(message));
+        return MiniMessage.miniMessage().deserialize(legacyToMiniMessage(legacyColor));
     }
 
     /**
